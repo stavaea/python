49694890b3598c1e74fd163d16e830a1fc79c3bb
waxberry <451009894@qq.com>
2024-08-22 15:20:26 +0800

    Initial commit

--- tools/a.py
+++ tools/a.py
@@ -8,14 +8,14 @@
 """
 import numpy
 
-# start1 = 5480
-# end1 = 9500
+start1 = 5480
+end1 = 9500
+
+a1 = numpy.linspace(start1, end1, num=81)
+print(a1)
+
+# start2 = 7600   #4540,23000,9800,6868,240,400,420
+# end2 = 9600   #9540,25000,11800,11868,2240,4400,4420
 #
-# a1 = numpy.linspace(start1, end1, num=41)
-# print(a1)
-
-start2 = 7600   #4540,23000,9800,6868,240,400,420
-end2 = 9600   #9540,25000,11800,11868,2240,4400,4420
-
-a2 = numpy.linspace(start2, end2, num=11)
-print(a2)
+# a2 = numpy.linspace(start2, end2, num=11)
+# print(a2)
